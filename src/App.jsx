08c582212bd1a897cd01b7be8bfb7057b1848011
MattiaAngelini <mattiaangelini1993@gmail.com>
2025-03-20 12:35:53 +0100

    Created products layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 import axios from 'axios'
+import './assets/styles/App.scss'
 
 function App() {
  const [products, setDProducts] = useState([])
@@ -13,15 +14,35 @@ function App() {
     .catch(error => console.error("Errore nel caricamento dei dati:", error));
 }, []);
 
+useEffect(()=>{
+console.log(products)
+},[products])
+
   return (
     <>
      <main>
-    <h1 className='text-center'>Prodotti</h1>
-    <ul>
-        {products.map((product, index) => 
-          <li key={index}>{product.title}</li>
-        )}
-    </ul>
+      <div className='d-flex justify-content-between'>
+        <h1 className='text-center'>Prodotti</h1>
+
+        <div className='cart mx-5 mt-3'>
+          <div>0</div>
+        </div>
+
+      </div>
+  
+      <section className='d-flex flex-wrap gap-1 justify-content-center p-5'>
+        {products.map((product, index) => (
+          <div key={index} className="ms-card m-3">
+            <img className='img-fluid' src={product.image} alt={product.title} />
+            <div className="card-body">
+              <h5 className="card-title">{product.title}</h5>
+              <p className="card-text">Prezzo: {product.price}</p>
+              <p className="card-text">Pezzi disponibili: {product.rating.count}</p>
+              <button className="btn btn-primary">AGGIUNGI AL CARRELLO</button>
+            </div>
+          </div>
+        ))}
+      </section>
     <nav>
         <Link to={`/Cart`} >CARRELLO</Link> 
       </nav>
